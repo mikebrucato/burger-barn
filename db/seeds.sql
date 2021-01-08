0064fc5ca6a1d9011a9b13acb1f80ed7ae831fad
mikebrucato <brucatomike1@gmail.com>
2021-01-08 10:15:21 -0600

    added burgers_db seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
