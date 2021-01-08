6155da323cbea61eb4bc303d400d342b87c361a0
mikebrucato <brucatomike1@gmail.com>
2021-01-08 11:28:03 -0600

    added db functions

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+// Import the ORM file to create functions that will interact with the burgers database
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res)
+        })
+    },
+
+    create: function(cols, values, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res)
+        })
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res)
+        })
+    },
+
+    delete: function(condition, cb) {
+        orm.delete("burgers", condition, function(res) {
+            cb(res)
+        })
+    }
+}
