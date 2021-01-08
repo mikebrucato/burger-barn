7c9d875b12aa82032f9161519ea315acc68ee10d
mikebrucato <brucatomike1@gmail.com>
2021-01-08 13:14:23 -0600

    added get route for index DOM

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,17 @@
 // Dependencies
 var express = require("express")
 
-var router = express.router
+var router = express.Router()
 
 var burger = require("../models/burger.js")
+
+// Renders Index to DOM
+router.get("/index", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = 
+        { burgers: data}
+        res.render("index", hbsObject)
+    })
+})
+
+module.exports = router
